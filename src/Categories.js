da86a8e87687348270245c458f7a8f8fefe56712
Zachary Sain <zachsain@gmail.com>
2022-04-28 14:03:44 -0500

    category progress

--- src/Categories.js
+++ src/Categories.js
@@ -1,12 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import CategoryForm from './CategoryForm'
 
 function Categories () {
+
+    const [categories, setCategories] = useState([])
+    const [categoryFormFlag, setCategoryFormFlag] = useState(false)
+
+    useEffect(() => {
+        fetch("http://localhost:9292/categories")
+        .then((r) => r.json())
+        .then(categories => (setCategories(categories), console.log(categories)))
+      },[])
 
     return (
         <div>
             <CategoryForm />
             <h3>Categories:</h3>
+            
         </div>
     )
 }
---
+++ src/DisplayCategories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DisplayCategories({name, image}){
+
+    return(
+        // <div>
+        //     <h1>{}</h1>
+        // </div>
+    )
+
+}
